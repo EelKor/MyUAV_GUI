333539af4e037071252744a12115931c45e383d6
SeungshinLee <lsver67@gmail.com>
2019-11-19 13:58:47 +0000

    GUI_EngienStatus : add Menu bar

--- GUI_EngineStatus.py
+++ GUI_EngineStatus.py
@@ -14,7 +14,24 @@ class GUI_EngineControl(GUI_base):
     def RPM_Indicator(self,_object):
         pass
         
+ 
+
+engine = GUI_EngineStatus()
+
+
+menu_bar = Menu(engine.win)
+engine.win.config(menu=menu_bar)
+com_menu = Menu(menu_bar, tearoff=0)
+com_menu.add_command(label="COM")
+com_menu.add_separator()
+com_menu.add_command(label="Ping Test")
+com_menu.add_separator()
+com_menu.add_command(label="Exit")
+menu_bar.add_cascade(label="Network", menu=com_menu)
 
 
 
-test = GUI_EngineControl()
+test_menu = Menu(menu_bar, tearoff=0)
+test_menu.add_command(label="Test")
+test_menu.add_command(label="Stop")
+menu_bar.add_cascade(label="Test", menu=test_menu)
